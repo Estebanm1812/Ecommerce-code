b908eaba943fc776a6ea7030d058b025cd30683a
estebanm1812 <EstebanMendoza02@hotmail.com>
2024-05-31 16:28:51 -0500

    idk

--- ecommerce-store-main/app/(routes)/cart/page.tsx
+++ ecommerce-store-main/app/(routes)/cart/page.tsx
@@ -3,16 +3,13 @@
 import { useEffect, useState } from 'react';
 
 import Container from '@/components/ui/container';
-import useCart from '@/hooks/use-cart';
 
-import Summary from './components/summary'
-import CartItem from './components/cart-item';
-
-export const revalidate = 0;
+import Summary from '../../../components/cart/summary'
+import CartItem from '../../../components/cart/cart-item';
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState(false);
-  const cart = useCart();
+  const cart:unknown[] = [];
 
   useEffect(() => {
     setIsMounted(true);
@@ -29,9 +26,9 @@ const CartPage = () => {
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
-              {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
+              {(cart as any)?.items?.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
               <ul>
-                {cart.items.map((item) => (
+                {(cart as any)?.items?.map((item:any) => (
                   <CartItem key={item.id} data={item} />
                 ))}
               </ul>
